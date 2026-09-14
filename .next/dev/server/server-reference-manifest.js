c3b42a2c98611145251f9b9dade004bbb25d8613
gerald-donkor <donkorgerald0@gmail.com>
2026-09-14 19:16:27 +0000

    build: update .gitignore and remove .next cache artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CYVvrar38KODGK3dnb5ccVs29pnIMbJZjxTK3lOAWYo=\"\n}"
--- .next/dev/static/chunks/_1hxlvs0._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_06lzag1._.css","static/chunks/_19g_l-a._.js","static/chunks/node_modules_next_dist_0b_t304._.js","static/chunks/app_design-system_design-system_module_1zv3zbx.css"],
-    source: "entry"
-});
